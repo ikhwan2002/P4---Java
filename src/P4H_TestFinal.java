c6c8ed73e87093a34a969e3b486f9f6ed559ea64
DELL <rizkiakmal37@gmail.com>
2022-03-28 16:33:57 +0700

    Menggunakan keyword final

---
+++ src/P4H_TestFinal.java
@@ -0,0 +1,15 @@
+
+
+public class P4H_TestFinal {
+
+    int x = 10;
+    double PI = 3.14;
+
+    public static void main(String[] args) {
+        P4H_TestFinal myObj = new P4H_TestFinal();
+        myObj.x = 50;
+        myObj.PI = 25;
+        System.out.println(myObj.x);
+    }
+}
+
