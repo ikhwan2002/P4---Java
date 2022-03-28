dd8c02e2d3e07a5d2d94d319797025b883c56b1f
DELL <rizkiakmal37@gmail.com>
2022-03-28 16:41:27 +0700

    Metode tanpa nilai kembali (void)

---
+++ src/P4I_MethodA.java
@@ -0,0 +1,8 @@
+public class P4I_MethodA {
+    public static void main(String[] args) {
+        welcome();
+    }
+    private static void welcome(){
+        System.out.println("Selamat belajar Java");
+    }
+}
