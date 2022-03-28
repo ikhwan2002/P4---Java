32c65397dd182657d7d8b53264050ccb6b979c4f
DELL <rizkiakmal37@gmail.com>
2022-03-28 15:37:23 +0700

    Mengimplementasikan class diagram (UML) menjadi kode sumber

---
+++ src/P4D_Mahasiswa.java
@@ -0,0 +1,28 @@
+public class P4D_Mahasiswa {
+    private String nama;
+    private int nrp;
+
+    public String getNama(){
+        return nama;
+    }
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    public int getNrp(){
+        return nrp;
+    }
+    public void setNrp(int nrp){
+        this.nrp = nrp;
+    }
+    public P4D_Mahasiswa(String nama,int nrp){
+        this.nama = nama;
+        this.nrp = nrp;
+    }
+    public void info(){
+        System.out.println(nama + "," + nrp);
+    }
+    public String toString(){
+        System.out.println("Data Mahasiswa\n" + nama + "\n" + nrp);
+        return null;
+    }
+}
