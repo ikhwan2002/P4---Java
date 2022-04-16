d9eef0de74f574e47ce3e2f1acc9c2cfccf0ac3b
DELL <rizkiakmal37@gmail.com>
2022-04-16 08:50:18 +0700

    Pemanggilan constructor yang memiliki 2 parameter

---
+++ src/P4N3_TestOrang.java
@@ -0,0 +1,18 @@
+public class P4N3_TestOrang {
+    public static void main(String[] args) {
+        P4N1_Orang badu = new P4N1_Orang();
+        badu.nama = "Badu";
+        badu.umur = 12;
+        System.out.println("Nama : " + badu.nama);
+        System.out.println("Umur : " + badu.umur + " tahun.");
+        badu.tertawa();
+
+        System.out.println();
+
+        P4N1_Orang dodo = new P4N1_Orang("Dodo", 10);
+        System.out.println("Nama : " + dodo.nama);
+        System.out.println("umur : " + dodo.umur + " tahun.");
+        dodo.menangis();
+        }
+    }
+
