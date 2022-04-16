5760f91f7b15c72bce179329fd17ad85823dbb46
DELL <rizkiakmal37@gmail.com>
2022-04-16 08:46:52 +0700

    Pemanggilam default constructor di kelas TestOrang

---
+++ src/P4N2_TestOrang.java
@@ -0,0 +1,10 @@
+public class P4N2_TestOrang {
+    public static void main(String[] args) {
+        P4N1_Orang badu = new P4N1_Orang();
+        badu.nama = "Badu";
+        badu.umur = 12;
+        System.out.println("Nama : " + badu.nama);
+        System.out.println("Umur : " + badu.umur + " tahun.");
+        badu.tertawa();
+    }
+}
