826d682363f51ae2caa030a6b76983e6c7849c58
DELL <rizkiakmal37@gmail.com>
2022-04-16 08:42:56 +0700

    Metode constructor

---
+++ src/P4N1_Orang.java
@@ -0,0 +1,22 @@
+public class P4N1_Orang {
+    String nama;
+    int umur;
+    String warnaRambut;
+    String warnaKulit;
+    int tinggiBadan;
+    int beratBadan;
+
+    public P4N1_Orang() {
+    }
+    public P4N1_Orang(String nama, int umur){
+        this.nama = nama;
+        this.umur = umur;
+    }
+        public void menangis(){
+            System.out.println("hu.... Hu....");
+        }
+        public void tertawa(){
+            System.out.println("Ha... Ha...");
+        }
+    }
+
